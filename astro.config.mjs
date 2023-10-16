d542348b08e2f40d3a40dbbf9eb728b7177268d4
Clément Lopez <clement@urbanease.io>
2023-10-16 14:49:51 +0200

    add analyitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "static",
   adapter: vercel(),
+  webAnalytics: {
+    enabled: true,
+  },
 });
