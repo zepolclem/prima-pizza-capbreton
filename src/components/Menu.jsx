679cdc09fb9483d2fbb29aab6261aa937cf414eb
zepolclem <lopez.clmnt@gmail.com>
2024-05-06 23:27:20 +0200

    style pepito

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,4 @@
-export default ({pizzaData}) => {
+export default ({ pizzaData }) => {
 
     const PizzaList = () => {
         return (
@@ -6,7 +6,7 @@ export default ({pizzaData}) => {
                 {pizzaData.data.map((pizza, index) => (
                     <li className="flex flex-row mt-3 items-end border-t-2" key={index}>
                         <div className="flex-1 p-3">
-                            <strong className="font-bold uppercase bg-slate-200 p-1 rounded font-serif">
+                            <strong className="font-bold uppercase bg-red-200 p-1">
                                 {pizza.name}
                             </strong>
                             <p className="text-sm md:text-base font-light pt-2">
@@ -24,11 +24,13 @@ export default ({pizzaData}) => {
 
     return (
         <div className="p-3 md:border-4 border-double">
-            <h2 className="font-bold text-2xl">Nos pizzas</h2>
-            <p className="text-xs text-slate-800">
-                Toutes nos pizzas sont accompagnées d'olives noires.<br />
-                Prix TTC.
-            </p>
+            <div className="flex flex-row justify-between">
+                <h2 className="font-bold text-2xl min-w-200">Nos Piiiizzas 🤌 </h2>
+                <p className="text-xs text-slate-800 text-right">
+                    Toutes nos pizzas sont accompagnées d'olives noires.<br />
+                    Prix TTC.
+                </p>
+            </div>
             <PizzaList />
         </div>
     );
