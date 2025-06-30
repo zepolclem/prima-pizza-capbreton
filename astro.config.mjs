a0322deaa8ee28656bd2b261550f5ae7db366140
zepolclem <lopez.clmnt@gmail.com>
2025-06-30 23:45:24 +0200

    vercel stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercelStatic from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
   site: "https://prima-pizza-capbreton.fr",
+  output: "static",
+  adapter: vercelStatic(),
   vite: {
     plugins: [tailwindcss()],
   },
+
   build: {
     // Génère les pages avec des URLs propres
     format: "directory"
   },
+
   compressHTML: true,
 });
