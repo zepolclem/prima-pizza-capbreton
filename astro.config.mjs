b343f6508494475697ab906b9664e7f7ee08ad68
zepolclem <lopez.clmnt@gmail.com>
2025-07-05 18:21:56 +0200

    Refactor de la configuration d'Astro pour intégrer Tailwind CSS via Vite, suppression de l'ancienne configuration de Tailwind, ajout de nouveaux fichiers CSS et mise à jour des fichiers HTML statiques pour le déploiement sur Vercel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,23 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
-    tailwind()
+    react()
   ],
+
   output: "static",
   adapter: vercel(),
 
   webAnalytics: {
     enabled: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
